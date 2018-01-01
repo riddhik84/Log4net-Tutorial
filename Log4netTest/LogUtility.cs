80a0f1098be3e0371fbff4d58377d56c7bb2a469
RKs <riddhik84@gmail.com>
2018-01-01 20:24:15 +0530

    log4net solution

---
+++ Log4netTest/LogUtility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+
+namespace Log4netTest
+{
+    class LogUtility
+    {
+        public static ILog logger;
+
+        public static void start()
+        {
+            //XmlConfigurator.Configure(new System.IO.FileInfo(System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "//Log4net.config"));
+            XmlConfigurator.ConfigureAndWatch(new System.IO.FileInfo(AppDomain.CurrentDomain.BaseDirectory + "log4net.config"));
+            logger = LogManager.GetLogger("LogFileAppenderSample");
+        }
+
+        public static void Debug(String msg)
+        {
+            logger.Debug(msg);
+        }
+
+    }
+}
---
+++ Log4netTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using log4net;
+using log4net.Config;
+
+namespace Log4netTest
+{
+    class Program
+    {
+        //private static readonly ILog log =
+        //log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        //public static ILog logger;
+
+        static void Main(string[] args)
+        {
+            LogUtility.start();
+
+            Console.WriteLine("Log4net test");
+
+            //logger = LogManager.GetLogger("LogFileAppenderSample");
+            for (int i = 0; i < 5; i++)
+            {
+                LogUtility.Debug("Test log file appender sample logger..." + i);
+            }
+
+            LogUtility.Debug(System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
+            LogUtility.Debug(AppDomain.CurrentDomain.BaseDirectory + "log4net.config");
+
+            Console.WriteLine("PRESS a key to close...");
+
+            //log.Info("Application is working");
+        }
+    }
+}
